f1e64d872f84f0801ecf0184a8173c9fe07c0bb9
Yuliya <yuliya.rostova@gmail.com>
2022-04-19 00:37:22 +0300

    feat: change local json to API

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
 const inputRub = document.getElementById('rub');
 const inputUsd = document.getElementById('usd');
 
+// I use API from https://www.cbr-xml-daily.ru/
+
 inputRub.addEventListener('input', () => {
   const request = new XMLHttpRequest();
 
-  request.open('GET', './js/current.json');
-  request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+  // request.open('GET', './js/current.json');
+  request.open('GET', 'https://www.cbr-xml-daily.ru/daily_json.js');
+  // request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
   request.send();
 
   request.addEventListener('load', () => {
     if (request.status === 200) {
       const data = JSON.parse(request.response);
-      console.log(data.current.usd);
-      inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+      console.log(data);
+      inputUsd.value = (+inputRub.value / data.Valute.USD.Value).toFixed(2);
     } else {
       console.log('Something is going wrong... Try again!')
     }
@@ -22,15 +25,15 @@ inputRub.addEventListener('input', () => {
 inputUsd.addEventListener('input', () => {
   const request = new XMLHttpRequest();
 
-  request.open('GET', './js/current.json');
-  request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+  // request.open('GET', './js/current.json');
+  request.open('GET', 'https://www.cbr-xml-daily.ru/daily_json.js');
+  // request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
   request.send();
 
   request.addEventListener('load', () => {
     if (request.status === 200) {
       const data = JSON.parse(request.response);
-      console.log(data.current.usd);
-      inputRub.value = (+inputUsd.value * data.current.usd).toFixed(2);
+      inputRub.value = (+inputUsd.value * data.Valute.USD.Value).toFixed(2);
     } else {
       console.log('Something is going wrong... Try again!')
     }

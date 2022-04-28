7989cab65301f8d5a864ab3fca1f3435d50928d3
Yuliya <yuliya.rostova@gmail.com>
2022-04-28 19:20:23 +0300

    refactor: merge similar code

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # calc-ajax-oldmethod
 Try to use methods working with ajax
 
-Step 1. Create simple currency calculator with local json file for RUB and USD
+Step 1. Create simple currency calculator with local json file and XMLHttpRequest for RUB and USD
 
 Step 2. Change json file to free API. I use API from https://www.cbr-xml-daily.ru/
+
+Step 3. Merge similar code
--- js/script.js
+++ js/script.js
@@ -1,36 +1,36 @@
-const inputRub = document.getElementById('rub');
-const inputUsd = document.getElementById('usd');
+const inputRUB = document.getElementById('rub');
+const inputBYR = document.getElementById('byn');
+const inputUSD = document.getElementById('usd');
 
-inputRub.addEventListener('input', () => {
-  const request = new XMLHttpRequest();
+const currencyArray = [inputRUB, inputBYR, inputUSD];
 
-  request.open('GET', 'https://www.cbr-xml-daily.ru/daily_json.js');
-  request.send();
+const request = new XMLHttpRequest();
+request.open('GET', 'https://www.cbr-xml-daily.ru/daily_json.js');
+request.send();
 
-  request.addEventListener('load', () => {
-    if (request.status === 200) {
-      const data = JSON.parse(request.response);
-      console.log(data);
-      inputUsd.value = (+inputRub.value / data.Valute.USD.Value).toFixed(2);
-    } else {
-      console.log('Something is going wrong... Try again!')
-    }
-  });
+request.addEventListener('load', () => {
+  if (request.status === 200) {
+    const data = JSON.parse(request.response);
+
+    currencyArray.forEach(currencyCur => {
+      currencyCur.addEventListener('input', () => {
+        currencyArray.forEach(currency => {  
+          if (currencyCur.id != currency.id) {
+            if (currencyCur.id === 'rub') {
+              currency.value = (+currencyCur.value / data.Valute[`${currency.id.toUpperCase()}`].Value).toFixed(2);
+            } else {
+              if (currency.id === 'rub') {
+                currency.value = (+currencyCur.value * data.Valute[`${currencyCur.id.toUpperCase()}`].Value).toFixed(2);
+              } else {
+                currency.value = ((+currencyCur.value * data.Valute[`${currencyCur.id.toUpperCase()}`].Value) / 
+                                data.Valute[`${currency.id.toUpperCase()}`].Value).toFixed(2);
+              }
+            }
+          } 
+        });
+      });
+    })
+  } else {
+    console.log('Something is going wrong... Try again!')
+  }
 });
-
-inputUsd.addEventListener('input', () => {
-  const request = new XMLHttpRequest();
-
-  request.open('GET', 'https://www.cbr-xml-daily.ru/daily_json.js');
-  request.send();
-
-  request.addEventListener('load', () => {
-    if (request.status === 200) {
-      const data = JSON.parse(request.response);
-      inputRub.value = (+inputUsd.value * data.Valute.USD.Value).toFixed(2);
-    } else {
-      console.log('Something is going wrong... Try again!')
-    }
-  });
-});
-
